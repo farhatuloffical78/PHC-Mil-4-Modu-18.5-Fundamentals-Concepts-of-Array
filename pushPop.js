a72220d3e5d7574835bbea4a5898e87e920103b7
farhatuloffical78 <mdfarhatulhaider@gmail.com>
2024-10-25 11:46:37 +0600

    updated

--- pushPop.js
+++ pushPop.js
@@ -4,4 +4,7 @@ const friends = ['kalam', 'jamal', 'karim', 'gelam', 'pailam'];
 friends.pop('jamal');
 console.log(friends);
 
+let out1 = friends.pop();
 
+console.log(friends);
+console.log(out1);
