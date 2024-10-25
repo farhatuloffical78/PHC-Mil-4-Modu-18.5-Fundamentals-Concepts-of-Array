17bc91b1ff5ea18bfe2cf338e6ddc66b84c05130
farhatuloffical78 <mdfarhatulhaider@gmail.com>
2024-10-25 20:01:18 +0600

    task2 added

---
+++ js-Array  Tasks/Task2.js
@@ -0,0 +1,22 @@
+/* 2. 
+    Add or remove elements
+    Declare an array of 3 tourist destinations
+    Add a new tourist destination to your tourist array
+    Add two more to your array
+    Remove the last tourist destination you have added
+    display the final array as output
+*/
+laces = ['Chittagong', 'Kaptai', 'Rangamati', 'Bandorban'];
+console.log(Places);
+
+Places.push('Coxs-Bazar', 'Comilla')
+console.log(Places);
+
+Places.push('Gazipur', 'Tazipur');
+console.log(Places);
+
+Places.pop()
+console.log(Places);
+
+
+
